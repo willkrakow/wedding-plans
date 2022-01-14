7451b5903413e4c28986e16ca299e1d47322928b
willkrakow <willkrakow@gmail.com>
2022-01-14 11:36:31 -0500

    Scrape data from Zola website with selenium. Source nodes for zola products. Create registry page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,6 @@
+const { createRemoteFileNode } = require('gatsby-source-filesystem')
+const puppeteer = require('puppeteer')
+
 exports.onCreatePage = async ({ page, actions }) => {
     const { createPage } = actions
     // page.matchPath is a special key that's used for matching pages
@@ -6,5 +9,103 @@ exports.onCreatePage = async ({ page, actions }) => {
         page.matchPath = "/app/*"
         // Update the page.
         createPage(page)
+    }
+}
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+    const { createNode } = actions
+    const ZOLA_URL = 'https://www.zola.com/registry/campbellkrakow'
+
+    // Data can come from anywhere, but for now create it manually
+    const browser = await puppeteer.launch({
+        headless: true,
+    })
+    const page = await browser.newPage()
+    await page.goto(ZOLA_URL)
+    await page.waitForSelector('.registry-collection-item-tile_container')
+    const resultsSelector = '.product-tile'
+    // Remove last item
+    
+    const data = await page.evaluate(resultsSelector => {
+        const tiles = Array.from(document.querySelectorAll(resultsSelector))
+        return tiles
+        .map(tile => {
+            const price = tile.querySelector('.price')?.innerText?.split("$")?.[1] || ""
+            const name = tile.querySelector('.register-item-name_entity-name')?.innerText || ""
+            const image = tile.querySelector('.entity-image picture img')?.getAttribute('src') || ""
+            const stillNeeds = tile.querySelector('.still-needs')?.innerText?.split(":")?.[1].trim() || ""
+            const productId = tile.querySelector('.registry-item-thumbnail')?.getAttribute('href')?.split('/')?.pop() || ""
+            return {
+                productId,
+                price,
+                name,
+                image,
+                stillNeeds
+            }
+        })
+        .filter((item) => item.name !== "Gift Card")
+    }, resultsSelector)
+
+    await browser.close()
+
+
+    data.forEach(item => {
+        const nodeMeta = {
+            id: createNodeId(`zola-product-${item.productId}`),
+            parent: null,
+            children: [],
+            internal: {
+                type: `ZolaProduct`,
+                content: JSON.stringify(item),
+                contentDigest: createContentDigest(item),
+                mediaType: `text/html`
+            }
+        }
+        const node = Object.assign({}, item, nodeMeta)
+        createNode(node)
+    })
+
+    await browser.close()
+}
+
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions
+    const typeDefs = `
+    type ZolaProduct implements Node {
+        id: ID!
+        productId: String!
+        price: String!
+        name: String!
+        image: String!
+        stillNeeds: String!
+        localImage: File @link(from: "fields.localFile")
+    }
+    `
+    createTypes(typeDefs)
+}
+
+exports.onCreateNode = async ({
+    node,
+    actions,
+    createNodeId,
+    getCache,
+}) => {
+    const { createNode, createNodeField } = actions
+    // For all MarkdownRemark nodes that have a featured image url, call createRemoteFileNode
+    if (
+        node.internal.type === "ZolaProduct" &&
+        node.image !== null
+    ) {
+        const fileNode = await createRemoteFileNode({
+            url: node.image, // string that points to the URL of the image
+            parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
+            createNode, // helper function in gatsby-node to generate the node
+            createNodeId, // helper function in gatsby-node to generate the node id
+            getCache,
+        })
+        // if the file was created, extend the node with "localFile"
+        if (fileNode) {
+            createNodeField({ node, name: "localFile", value: fileNode.id })
+        }
     }
 }
--- src/pages/registry.tsx
+++ src/pages/registry.tsx
@@ -1,36 +1,102 @@
 import React from 'react'
-import { Container } from 'reactstrap'
-import styled from 'styled-components'
-import { H2, H4 } from '../components/typography'
+import { Col, Container, Row } from 'reactstrap'
+import { H2, H3, H4, H5, P } from '../components/typography'
 import './registry.css'
+import { IGatsbyImageData, GatsbyImage } from 'gatsby-plugin-image'
+import { PageProps, graphql } from 'gatsby'
+import Button from '../components/button'
+import styled from 'styled-components'
 
-const ZolaHide = styled.div`
-border-bottom: 150px solid ${(props) => props.theme.colors.background};
+type LocalImage = {
+  childImageSharp: {
+    gatsbyImageData: IGatsbyImageData
+  }
+}
+
+type ZolaProduct = {
+  id: string;
+  name: string;
+  price: string;
+  image: string;
+  localImage: LocalImage;
+  productId: string;
+  stillNeeds: string;
+}
+
+interface Props {
+    allZolaProduct: {
+      nodes: ZolaProduct[]
+    }
+}
+
+const DetailsBox = styled.div`
+flex: 1;
+margin-top: ${(props) => props.theme.spacing[3]};
+margin-bottom: ${(props) => props.theme.spacing[3]};
 `
 
-const Registry = () => {
-  React.useEffect(() => {
-    document.createElement('script')
-    const script = document.createElement('script')
-    script.innerHTML = `!function(e,t,n){var s,a=e.getElementsByTagName(t)[0];e.getElementById(n)||(s=e.createElement(t),s.id=n,s.async=!0,s.src="https://widget.zola.com/js/widget.js",a.parentNode.insertBefore(s,a))}(document,"script","zola-wjs");`;
+const Price = styled(H5)`
+font-family: ${(props) => props.theme.fonts.body};
+font-weight: bold;
+font-size: ${(props) => props.theme.fontSizes[2]};
+}`
 
-    document.body.appendChild(script)
+const Name = styled(H3)`
+font-size: ${(props) => props.theme.fontSizes[3]};
+`
+type RegistryPageProps = PageProps<Props>
 
-  }, [])
+const Registry = ({ data }: RegistryPageProps) => {
+  const {nodes: products} = data.allZolaProduct
   return (
     <Container>
       <H2 centered>Registry</H2>
       <H4 centered alwaysdark inline></H4>
-      <ZolaHide />
-      <a
-        className="zola-registry-embed"
-        href="www.zola.com/registry/campbellkrakow"
-        data-registry-key="campbellkrakow"
-      >
-        Our Zola Wedding Registry
-      </a>
+      <Container>
+        <Row>
+          {products.map((product) => (
+            <Col className="my-3" xs={12} md={6} lg={3} key={product.id}>
+              <article className="d-flex flex-column h-100">
+                <GatsbyImage
+                  image={product.localImage.childImageSharp.gatsbyImageData}
+                  alt={product.name}
+                />
+                <DetailsBox>
+                  <Name>{product.name}</Name>
+                </DetailsBox>
+                <Price>${product.price}</Price>
+                <P>Still need: {product.stillNeeds}</P>
+                <a
+                  href={`https://www.zola.com/registry/collection-item/${product.productId}`}
+                >
+                  <Button>View and buy</Button>
+                </a>
+              </article>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </Container>
   );
 }
 
 export default Registry
+
+export const query = graphql`
+  {
+    allZolaProduct {
+      nodes {
+        localImage {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        id
+        name
+        price
+        stillNeeds
+        productId
+      }
+    }
+  }
+`;
