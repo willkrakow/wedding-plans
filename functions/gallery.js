8bcae0162614fed938d4638dc6038cc2ef55b7f3
willkrakow <willkrakow@gmail.com>
2022-01-18 23:36:07 -0500

    Async fetching of guest photos. Pagination of guest photos.

--- functions/gallery.js
+++ functions/gallery.js
@@ -1,6 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
 const Airtable = require("airtable");
+const fetch = require("node-fetch");
+
 
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
@@ -17,54 +19,75 @@ const base = new Airtable({
 }).base(process.env.AIRTABLE_BASE_ID);
 
 function formatDate(date) {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 exports.handler = async (event, context) => {
   if (event.httpMethod === "POST") {
     const data = JSON.parse(event.body);
-    
+
     try {
-    const imageUpload = await cloudinary.uploader.upload(data.image, {
-        context: {caption: data.caption},
-    });
+      const imageUpload = await cloudinary.uploader.upload(data.image, {
+        folder: "Wedding",
+        context: { caption: data.caption },
+      });
       if (imageUpload) {
         const imageResponse = {
           ...imageUpload,
           caption: data.caption,
         };
 
-        const currentTime = new Date()
-        const date = formatDate(currentTime)
+        const currentTime = new Date();
+        const date = formatDate(currentTime);
 
         const imageRecord = await base("Photos").create({
-            src: [{ url: imageUpload.url }],
-            description: data.caption,
-            title: "Wedding day!",
-            location: "Hillsborough, NC",
-            date,
+          src: [{ url: imageUpload.url }],
+          description: data.caption,
+          title: "Wedding day!",
+          location: "Hillsborough, NC",
+          date,
         });
 
         return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "Image uploaded successfully",
-                data: imageResponse
-            }),
-        }
+          statusCode: 200,
+          body: JSON.stringify({
+            message: "Image uploaded successfully",
+            data: imageResponse,
+          }),
+        };
       }
+    } catch (error) {
+      console.log(error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "File upload failed",
+          data: {},
+        }),
+      };
+    }
+  }
 
-
-    //     console.log(imageRecord);
-    //     return {
-    //       statusCode: 200,
-    //       body: JSON.stringify({
-    //         message: "File uploaded",
-    //         data: {
-    //           ...imageResponse,
-    //         },
-    //       }),
-    //     };
-    //   }
+  if (event.httpMethod === "GET") {
+      const next_cursor = event?.queryStringParameters?.next_cursor || null;
+    try {
+      // Get images from cloudinary folder
+      const options = {
+        type: "upload",
+        prefix: "Wedding",
+        transformation: [{ width: 500, crop: "fill" }],
+        max_results: 3,
+      };
+        if (next_cursor) {
+            options.next_cursor = next_cursor;
+        }
+      const images = await cloudinary.api.resources(options);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Images retrieved successfully",
+          data: images,
+        }),
+      };
     } catch (error) {
       console.log(error);
       return {
--- src/pages/guest_photos.tsx
+++ src/pages/guest_photos.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import { Container } from 'reactstrap'
-import { H2 } from '../components/typography'
-export default function GuestPhotos() {
-    return (
-        <Container>
-            <H2>Guest photos</H2>
-        </Container>
-    )
-}
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { H2, H4 } from "../components/typography";
+import { WhiteButton } from '../components/button'
 
+type CloudinaryPhoto = {
+  asset_id: string;
+  public_id: string;
+  format: "png" | "jpg" | "jpeg" | "webp";
+  version: number;
+  resource_type: "image";
+  type: "upload";
+  created_at: string;
+  bytes: number;
+  width: number;
+  height: number;
+  url: string;
+  secure_url: string;
+};
+
+export default function GuestPhotos() {
+  const [photos, setPhotos] = React.useState<CloudinaryPhoto[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [nextCursor, setNextCursor] = React.useState<string | null>(null);
+
+  const fetchPhotos = React.useCallback(async () => {
+    setLoading(true);
+    const res = await fetch(
+      `/.netlify/functions/gallery?${
+        nextCursor ? `&next_cursor=${nextCursor}` : ""
+      }`
+    ).then((res) => res.json());
+
+    setNextCursor(res.data?.next_cursor || null );
+    console.log(res.data);
+    const allPhotos = [...photos, ...res.data.resources];
+    setPhotos(allPhotos);
+    setLoading(false);
+  }, [photos, nextCursor]);
+
+  React.useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container>
+      <H2 centered>Guest photos</H2>
+      <Row>
+        {photos.map((photo: CloudinaryPhoto) => (
+          <Col key={photo.public_id} xs={12} md={4}>
+            <img
+              style={{ width: "100%", height: "auto" }}
+              src={photo.secure_url}
+            />
+          </Col>
+        ))}
+        {loading && (
+          <Col xs={12}>
+            <H4 centered alwaysdark inline>
+              Loading...
+            </H4>
+          </Col>
+        )}
+
+        {nextCursor && (
+            <Col xs={12}>
+                <WhiteButton onClick={fetchPhotos}>Load more</WhiteButton>
+            </Col>
+        )}
+      </Row>
+    </Container>
+  );
+}
